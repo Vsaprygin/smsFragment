6bfc87036eee043e8f15d5f53acee6851581b7f2
Виталий Сапрыгин <saprygin@altasib.ru>
2022-12-07 14:12:58 +0700

    HomeWork2

---
+++ app/src/main/java/com/vsaprygin/smsfragment/SettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.vsaprygin.smsfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.vsaprygin.smsfragment.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.advansedBtn.setOnClickListener {
+            binding.codeSMS.text = "Кнопка сработала"
+//            Navigation.findNavController(binding.root).navigate(R.id.action_settingsFragment_to_advansedFragment)
+        }
+    }
+}
